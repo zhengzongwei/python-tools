7b43db58e71fd79a010411a314073a7de359989c
zhengzongwei <zhengzongwei@foxmail.com>
2021-07-24 12:49:38 +0800

    添加查询数据库相关操作

--- common/mariadb.py
+++ common/mariadb.py
@@ -21,16 +21,65 @@ class Mariadb(object):
             print(f"Error connecting to MariaDB Platform: {e}")
             quit()
         else:
-            self.cur = self.conn.cursor()
+            if self.conn is not None:
+                self.cur = self.conn.cursor()
 
     def execute_sql(self, sql):
-        try:
-            self.cur.execute(sql)
-        except mariadb.Error as e:
-            # TODO 使用日志记录相关错误
-            print(f"Error connecting to MariaDB Platform: {e}")
-            quit()
-        return self.cur.fetchall()
+        if self.cur is not None:
+            try:
+                self.cur.execute(sql)
+            except mariadb.Error as e:
+                # TODO 使用日志记录相关错误
+                print(f"Error connecting to MariaDB Platform: {e}")
+                quit()
+            return self.cur
+
+    def fetch_all(self, sql):
+        if self.cur is not None:
+            try:
+                self.execute_sql(sql)
+                db_data = self.cur.fetachall()
+            except mariadb.Error as e:
+                # TODO 使用日志记录相关错误
+                print(f"Error connecting to MariaDB Platform: {e}")
+                quit()
+            else:
+                return db_data
+
+    # def fetch_one(self,sql):
+    #     if self.cur is not None:
+    #         try:
+    #             self.execute_sql(sql)
+    #             db_data = self.cur.fetachone()
+    #         except mariadb.Error as e:
+    #             # TODO 使用日志记录相关错误
+    #             print(f"Error connecting to MariaDB Platform: {e}")
+    #             quit()
+    #         else:
+    #             return db_data
+
+    def update(self, sql):
+
+        if self.cur is not None:
+            try:
+                self.execute_sql(sql)
+                self.conn.commit()
+            except mariadb.Error as e:
+                self.conn.rollback()
+                print(f"Update data Failed, rollback")
+                return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
 
     def __del__(self):
         self.cur.close()
