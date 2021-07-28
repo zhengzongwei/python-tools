40765be7d450300635a58e4e1cced84ada0398d0
zhengzongwei <zhengzongwei@foxmail.com>
2021-07-28 13:29:33 +0800

    添加mongo 类初始化方法

--- common/mongodb.py
+++ common/mongodb.py
@@ -9,3 +9,15 @@ import pymongo
 # author: zhengzongwei@foxmail.com
 ################################################################
 """
+
+
+class MongoDB(object):
+    def __init__(self, ip, port, db):
+        self.ip = ip if ip else "localhost"
+        self.port = port if port else 27017
+        uri = 'mongodb://%s:%s/' % (self.ip, self.port)
+        self.client = pymongo.MongoClient(uri)
+        print(self.client)
+
+    def __del__(self):
+        self.client.close()
