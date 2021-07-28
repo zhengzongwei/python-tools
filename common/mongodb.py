7f12ca8600a7e2586bf8ed09d796fd01bba8d64a
zhengzongwei <zhengzongwei@foxmail.com>
2021-07-28 11:07:24 +0800

    添加 mongo数据库支持

---
+++ common/mongodb.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pymongo
+
+"""
+################################################################
+# description: MongoDB 数据库的读写操作
+# author: zhengzongwei@foxmail.com
+################################################################
+"""
