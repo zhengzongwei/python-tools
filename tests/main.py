b775ea562cb10dfd4a6b9286c19c6b665c554873
zhengzongwei <zhengzongwei@foxmail.com>
2022-03-07 16:03:10 +0800

    add unit test

--- tests/main.py
+++ tests/main.py
@@ -2,23 +2,23 @@ import sys
 
 sys.path.append("..")
 
-from unittest import mock
+# from unittest import mock
 
-from common.common import *
+from common.common import time_stamp
 from common.logs import Logger
 
 
-# 装饰器
-def user_logging(func):
-    print("11111")
-    def wrapper(*args,**kwargs):
-        raise ValueError
-        print("2222 %s",func.__name__)
-        print(args)
+# # 装饰器
+# def user_logging(func):
+#     print("11111")
+#     def wrapper(*args,**kwargs):
+#         raise ValueError
+#         print("2222 %s",func.__name__)
+#         print(args)
 
-        return func(*args)
-    print(("33333"))
-    return wrapper
+#         return func(*args)
+#     print(("33333"))
+#     return wrapper
 
 # @user_logging
 # def foo(sd):
@@ -26,7 +26,7 @@ def user_logging(func):
 if __name__ == '__main__':
     # foo("ssd")
 
-    # print(now_time_strap())
+    print(time_stamp())
     # print(format_time())
     # format_print(str(time_stamp()))
     # print(format_timestamp(str(time_stamp())))
@@ -37,9 +37,3 @@ if __name__ == '__main__':
     # print(time_stamp())
 
     # MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True)
-
-
-    # controllers = [con for con in nodes if con.get('is_controller')]
-    # if len(controllers) != 1 and len(controllers) != 3:
-    #     raise ValidationError(_("Controllers count must be 1 or 3"))
-    pass
