097e44d9ceda8164130eb3344dad52b582101859
zhengzongwei <zhengzongwei@foxmail.com>
2024-03-01 15:26:51 +0800

    更新日志模块

--- utils/logger.py
+++ utils/logger.py
@@ -1,114 +1,118 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# @Time    : 2024/2/4 14:08
+# @Time    : 2024/2/28 15:17
 # @Author  :  zhengzongwei<zhengzongwei@foxmail.com>
 
 import logging
 import os
-from datetime import datetime
+from logging.handlers import TimedRotatingFileHandler
 
 import toml
-from logging.handlers import TimedRotatingFileHandler
+
+LOG_LEVEL_MAPPING = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL,
+    "FATAL": logging.FATAL
+}
+
+
+class LogConfigError(Exception):
+    message = "Error loading logging configuration: %(log_config)s: %(message)s"
+
+    def __init__(self, log_config, message):
+        self.log_config = log_config
+        self.err_msg = message
+
+    def __str__(self):
+        return self.message % dict(log_config=self.log_config, message=self.err_msg)
 
 
 class Logger(object):
-    """
-    基础日志类
-    """
-    log_level = "info"
-    log_format = "%(asctime)s - %(name)s[func: %(funcName)s line:%(lineno)d] - %(levelname)s: %(message)s"
-    log_console = True
-    stacklevel = 2
+    base_path = os.path.dirname(os.path.dirname(__file__))
 
-    def __init__(self, logger_name, logger_path="logs.toml") -> None:
-        self.base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        print(self.base_dir)
-        self.log_config = dict()
+    default_log_config = {
+        "level": "debug",
+        "log_dir": "",
+        "log_file": "",
+        "log_format": "%(asctime)s %(process)d %(levelname)s %(name)s %(message)s [%(funcName)s] %("
+                      "pathname)s:%(lineno)d",
+        "log_console": True
+
+    }
+
+    def __init__(self, logger_name=None):
         self.logger = None
-        self.logger_conf = dict()
-
-        self.logger_format = None
+        self.logger_conf_path = None
 
         self.logger_name = logger_name
-        self.logger_path = logger_path
+        self.logger_conf = self.default_log_config
+        self._get_log_conf_path()
+        self._setup_logger()
 
-        self._get_log_conf()
-        self.stacklevel = self.logger_conf['stacklevel']
-        self.logger_conf_level = self.logger_conf['log_level']
-        self.logger_level = self.parse_log_level(self.logger_conf_level.upper())
+    def _get_log_conf_path(self):
+        logger_conf_path = os.path.join(self.base_path, 'conf', 'logs.toml')
+        if os.path.exists(logger_conf_path):
+            self.logger_conf_path = logger_conf_path
 
-        self.logger_format = self.get_conf_format()
-        self.init_logger()
-
-    def _get_log_conf(self) -> None:
-        try:
-            log_conf = toml.load(self.logger_path)
-            self.logger_conf = log_conf.get("logs", {})
-        except:
-            self.logger_conf = {
-                'log_level': self.log_level,
-                'log_format': self.log_format,
-                'stacklevel': self.stacklevel,
-                'log_console': self.log_console
-            }
-
-    def get_conf_format(self):
-        logger_format = "%(asctime)s - %(name)s[func: %(funcName)s line:%(lineno)d] - %(levelname)s: %(message)s"
-        return self.logger_conf.get("format", logger_format)
-
-    @staticmethod
-    def parse_log_level(level: str) -> int:
-        levels = {
-            "INFO": logging.INFO,
-            "DEBUG": logging.DEBUG,
-            "WARNING": logging.WARNING,
-            "ERROR": logging.ERROR,
-            "CRITICAL": logging.CRITICAL
-        }
-        return levels.get(level, logging.WARNING)
-
-    def init_logger(self) -> None:
+    def _setup_logger(self):
         self.logger = logging.getLogger(self.logger_name)
-        self.logger.setLevel(self.logger_level)
 
-        # 是否打印在console
-        if not self.logger_conf.get('log_console', False):
-            console_handle = logging.StreamHandler()
-            console_handle.setFormatter(logging.Formatter(self.logger_format))
-            self.logger.addHandler(console_handle)
+        if self.logger_conf_path is not None:
+            self._get_log_config()
 
-        if not self.logger_conf.get('log_path'):
-            log_path = os.path.join(self.base_dir, 'logs', 'log-%s.log' % datetime.now().strftime('%Y-%m-%d'))
-            self.make_conf_dir(os.path.dirname((log_path)))
+        self.logger.setLevel(LOG_LEVEL_MAPPING[self.logger_conf.get("level", "INFO").upper()])
+
+        # 如果是debug模式 或者开启了 log_console 则打印日志在console
+        if self.logger_conf.get("log_console", False) or self.logger_conf.get("level").lower() == "debug":
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(logging.Formatter(self.logger_conf.get("log_format")))
+            self.logger.addHandler(console_handler)
+
+        if self.logger_conf_path is not None:
+            log_path = self._get_log_file_path()
             file_handler = TimedRotatingFileHandler(log_path, when="midnight", interval=1, backupCount=3,
                                                     encoding='utf-8')
-            file_handler.setFormatter(logging.Formatter(self.logger_format))
+            file_handler.setFormatter(logging.Formatter(self.logger_conf.get("log_format")))
             self.logger.addHandler(file_handler)
 
-    def make_conf_dir(self,path) -> None:
-        if not os.path.exists(path):
-            os.makedirs(path, exist_ok=True)
-    def _log(self, msg):
-        getattr(self.logger, self.logger_conf_level)(msg, stacklevel=self.stacklevel)
+    def _get_log_config(self):
+        try:
+            log_conf = toml.load(self.logger_conf_path)
+            self.logger_conf = log_conf.get('logs', {})
+        except LogConfigError as e:
+            logging.warning("No logging configuration,Use default logging configuration!")
+        return self.logger_conf
 
-    def debug(self, msg):
-        self._log(msg)
+    def _get_log_file_path(self):
+        log_dir = self.logger_conf.get("log_dir")
+        log_file = self.logger_conf.get("log_file")
 
-    def info(self, msg):
-        self._log(msg)
+        if not log_dir or not log_file:
+            log_conf_path = os.path.join(self.base_path, "logs")
+            if not os.path.exists(log_conf_path):
+                os.makedirs(log_conf_path)
+            return os.path.join(log_conf_path, '%s.log' % self.logger_name)
 
-    def warning(self, msg):
-        self._log(msg)
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
 
-    def error(self, msg):
-        self._log(msg)
+        if log_dir and log_file:
+            return os.path.join(log_dir, log_file)
+        if log_file and not log_dir:
+            return os.path.join(self.base_path, log_file)
+        if log_dir:
+            return '%s.log' % os.path.join(log_dir, self.logger_name)
 
-    def critical(self, msg):
-        self._log(msg)
+        return None
+
+    def getLogger(self):
+        return self.logger
 
 
 if __name__ == '__main__':
-    # log = Logger("test123")
-    # log.error("12331234")
-    print(os.path.dirname(__file__))
+    LOG = Logger(__name__).getLogger()
+
+    LOG.error('hello world')
