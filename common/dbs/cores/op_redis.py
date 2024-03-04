9fdc0da66f4aafa32028259228ddcd886c59831e
zhengzongwei <zhengzongwei@foxmail.com>
2024-03-04 11:37:35 +0800

    更新redis 读写模块 demo

--- common/dbs/cores/op_redis.py
+++ common/dbs/cores/op_redis.py
@@ -4,18 +4,24 @@
 # @Time    : 2024/2/1 15:02                              
 # @Author  :  zhengzongwei<zhengzongwei@foxmail.com>
 
-
 import redis
 
+from utils.logger import Logger
 
+LOG = Logger("redis").getLogger()
 class RedisClient(object):
     def __init__(self, username=None, password=None, host='localhost', port=6379, db=0):
         try:
             self.redis_conn = redis.Redis(username=username, password=password, host=host, port=port, db=db)
+            LOG.info("redis client connected")
+            # 尝试执行一个简单的操作来验证连接
+            if not self.redis_conn.ping():
+                LOG.error("Failed to ping Redis server: Connection is not healthy")
+                return
         except redis.AuthenticationError:
-            print(f"Redis authentication")
+            LOG.error(f"Redis authentication")
         except Exception as e:
-            print("Failed to connect to Redis: %s", str(e))
+            LOG.error("Failed to connect to Redis: %s" % str(e))
 
     def set(self, key, value):
         return self.redis_conn.set(key, value)
